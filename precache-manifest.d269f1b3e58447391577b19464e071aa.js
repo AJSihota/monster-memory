08af7b3ea05a74f95dfe6de242fb11933e3e8713
user <user@users-MacBook-Pro-2.local>
2019-08-25 12:53:13 -0600

    Updates

---
+++ precache-manifest.d269f1b3e58447391577b19464e071aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ce782e7ce1e1d849c4e5455604e010",
+    "url": "/monster-memory/index.html"
+  },
+  {
+    "revision": "9df9d358df9fbc8b178d",
+    "url": "/monster-memory/static/css/main.fdf95bb6.chunk.css"
+  },
+  {
+    "revision": "7c7d27cf0f9ae0dc6e79",
+    "url": "/monster-memory/static/js/2.6ed2e688.chunk.js"
+  },
+  {
+    "revision": "9df9d358df9fbc8b178d",
+    "url": "/monster-memory/static/js/main.378dc2b1.chunk.js"
+  },
+  {
+    "revision": "6e48e647050da25fff97",
+    "url": "/monster-memory/static/js/runtime~main.19bf3515.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-memory/static/media/logo.5d5d9eef.svg"
+  }
+]);
